0dd2b9ca37a16481d5a85bc9369156dda0bcbc2f
Blair <blair@mcmillan.id.au>
2013-08-21 19:52:29 +1000

    Initial start of design

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VisionMixer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Settings settings = new Settings();
+            settings.ShowDialog();
+        }
+
+        private void Starcraft2Button_Click(object sender, RoutedEventArgs e)
+        {
+            Starcraft2 starcraft2 = new Starcraft2();
+            starcraft2.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Starcraft2.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VisionMixer
+{
+    /// <summary>
+    /// Interaction logic for Starcraft2.xaml
+    /// </summary>
+    public partial class Starcraft2 : Window
+    {
+        public Starcraft2()
+        {
+            InitializeComponent();
+        }
+    }
+}
